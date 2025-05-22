34dbb032bddbe0108885f48847fb5f355a224231
myself <3156555720@qq.com>
2025-05-22 20:21:34 +0800

    初始化提交

---
+++ src/main/java/com/reliccollider/hengqimall/HengQiMallApplication.java
@@ -0,0 +1,13 @@
+package com.reliccollider.hengqimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HengQiMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HengQiMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reliccollider/hengqimall/HengQiMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reliccollider.hengqimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HengQiMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
